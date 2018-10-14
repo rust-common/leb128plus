fd2b09153d50eea26b64a64e27f39125ddff3043
Sergey Shandar <sergey.shandar@gmail.com>
2018-10-13 19:06:35 -0700

    0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,4 @@
-pub fn to_leb128<F>(mut v: u64, write: &mut F) 
-where F: FnMut(u8) {
-    loop {
-        let x = v as u8;      
-        v = v >> 7;
-        if v == 0 {
-            write(x);
-            break;
-        }
-        write(0x80 | x);
-    }
-}
-
-pub fn from_leb128_plus<F>(get: &mut F) -> u64 
+pub fn from<F>(get: &mut F) -> u64 
 where F: FnMut() -> u8 {
     let mut result = 0;
     let mut shift = 0;
@@ -25,7 +12,7 @@ where F: FnMut() -> u8 {
     }
 }
 
-pub fn to_leb128_plus<F>(mut v: u64, write: &mut F)
+pub fn to<F>(mut v: u64, write: &mut F)
 where F: FnMut(u8) {
     loop {
         let x = v as u8;      
@@ -43,30 +30,10 @@ where F: FnMut(u8) {
 mod tests {
     use super::*;
 
-    fn test_leb128(value: u64, values: &[u8]) {
-        let mut i = 0;
-        to_leb128(
-            value,
-            &mut |q| {
-                assert_eq!(q, values[i]);
-                i = i + 1;
-            }
-        );
-        assert_eq!(i, values.len()) 
-    }
-
-    #[test]
-    fn leb128() {
-        test_leb128(0, &[0]);
-        test_leb128(127, &[127]);
-        test_leb128(128, &[128, 1]);
-        test_leb128(0xFF, &[0xFF, 1]);
-    }
-
     fn test_leb128_plus(value: u64, values: &[u8]) {
         {
             let mut i = 0;
-            to_leb128_plus(
+            to(
                 value,
                 &mut |q| {
                     assert_eq!(q, values[i]);
@@ -77,7 +44,7 @@ mod tests {
         }
         {
             let mut i = 0;
-            let result = from_leb128_plus(&mut || {
+            let result = from(&mut || {
                 let q = values[i];
                 i = i + 1;
                 return q;
