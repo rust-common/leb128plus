556b3c0c9e0739866449147b8b5f6a1bf965d4d0
Sergey Shandar <sergey.shandar@gmail.com>
2018-11-17 12:48:25 -0800

    Another iteration

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,25 @@
+extern crate num_traits;
+
+use num_traits::sign::Unsigned;
+use num_traits::int::PrimInt;
+use num_traits::cast::AsPrimitive;
+
 pub trait Write {
-    fn write(&mut self, v: u64) -> std::io::Result<&mut Self>;
+    fn write<V>(&mut self, v: V) -> std::io::Result<&mut Self>
+        where V: Unsigned + PrimInt + AsPrimitive<u8>;
+
+    fn write_u8(&mut self, v: u8) -> std::io::Result<&mut Self> { self.write(v) }
+
+    fn write_u16(&mut self, v: u16) -> std::io::Result<&mut Self> { self.write(v) }
+
+    fn write_u32(&mut self, v: u32) -> std::io::Result<&mut Self> { self.write(v) }
+
+    fn write_u64(&mut self, v: u64) -> std::io::Result<&mut Self> { self.write(v) }
+
+    fn write_u128(&mut self, v: u128) -> std::io::Result<&mut Self> { self.write(v) }
 }
 
-/// Write `u64` in the LEB128+ format to `std::io::Write` stream.
+/// Write unsigned integer in the LEB128+ format to `std::io::Write` stream.
 ///
 /// ## Examples
 ///
@@ -11,13 +28,13 @@ pub trait Write {
 ///     let mut v = vec![];
 ///     use leb128plus::Write;
 ///     std::io::Cursor::new(&mut v)
-///         .write(0)?
-///         .write(127)?
-///         .write(128)?
-///         .write(0xFF)?
-///         .write(0x17F)?
-///         .write(0x407F)?
-///         .write(0x4080)?;
+///         .write_u8(0)?
+///         .write_u16(127)?
+///         .write_u32(128)?
+///         .write_u64(0xFF)?
+///         .write_u128(0x17F)?
+///         .write_u16(0x407F)?
+///         .write(0x4080_u32)?;
 ///     Ok(v)
 /// };
 /// assert_eq!(
@@ -34,22 +51,30 @@ pub trait Write {
 /// );
 /// ```
 impl<T: std::io::Write> Write for T {
-    fn write(&mut self, mut v: u64) -> std::io::Result<&mut Self> {
+    fn write<V>(&mut self, mut v: V) -> std::io::Result<&mut Self>
+        where V: Unsigned + PrimInt + AsPrimitive<u8>
+    {
         loop {
-            let x = v as u8;
+            let x = v.as_();
             v = v >> 7;
-            if v == 0 {
+            if v.is_zero() {
                 self.write(&[x])?;
                 break Ok(self);
             }
             self.write(&[0x80 | x])?;
-            v = v - 1;
+            v = v - V::one();
         }
     }
 }
 
 pub trait Read {
-    fn read(&mut self) -> std::io::Result<u64>;
+    fn read<V>(&mut self) -> std::io::Result<V>
+        where V: 'static + PrimInt + Unsigned, u8: AsPrimitive<V>;
+    fn read_u8(&mut self) -> std::io::Result<u8> { self.read() }
+    fn read_u16(&mut self) -> std::io::Result<u16> { self.read() }
+    fn read_u32(&mut self) -> std::io::Result<u32> { self.read() }
+    fn read_u64(&mut self) -> std::io::Result<u64> { self.read() }
+    fn read_u128(&mut self) -> std::io::Result<u128> { self.read() }
 }
 
 /// Read `u64` in the LEB128+ format from `std::io::Read` stream.
@@ -68,26 +93,30 @@ pub trait Read {
 ///     128,
 /// ]);
 /// use leb128plus::Read;
-/// assert_eq!(c.read().unwrap(), 0);
-/// assert_eq!(c.read().unwrap(), 127);
-/// assert_eq!(c.read().unwrap(), 128);
-/// assert_eq!(c.read().unwrap(), 0xFF);
-/// assert_eq!(c.read().unwrap(), 0x17F);
-/// assert_eq!(c.read().unwrap(), 0x407F);
-/// assert_eq!(c.read().unwrap(), 0x4080);
-/// assert!(match c.read() {
+/// assert_eq!(c.read_u8().unwrap(), 0);
+/// assert_eq!(c.read_u16().unwrap(), 127);
+/// assert_eq!(c.read_u32().unwrap(), 128);
+/// assert_eq!(c.read_u64().unwrap(), 0xFF);
+/// assert_eq!(c.read_u128().unwrap(), 0x17F);
+/// assert_eq!(c.read_u16().unwrap(), 0x407F);
+/// assert_eq!(c.read_u32().unwrap(), 0x4080);
+/// assert!(match c.read_u64() {
 ///     Result::Err(_) => true,
 ///     _ => false
 /// });
 /// ```
 impl<T: std::io::Read> Read for T {
-    fn read(&mut self) -> std::io::Result<u64> {
-        let mut result = 0;
+    fn read<V>(&mut self) -> std::io::Result<V>
+        where
+            V: 'static + PrimInt + Unsigned,
+            u8: AsPrimitive<V>
+    {
+        let mut result: V = V::zero();
         let mut shift = 0;
         loop {
             let mut v = [0];
             self.read_exact(&mut v)?;
-            result += (v[0] as u64) << shift;
+            result = result + ((v[0].as_()) << shift);
             if v[0] < 128 {
                 break Ok(result);
             }
