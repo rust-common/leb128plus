c1a4c6d100e9e971de60f23e3f171b44fcd8ec28
Sergey Shandar <sergey.shandar@gmail.com>
2018-11-17 17:44:00 -0800

    using int

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,8 @@
+extern crate int;
 extern crate num_traits;
 
+use int::UInt;
 use num_traits::cast::AsPrimitive;
-
-pub trait UInt where
-    Self: AsPrimitive<u8> +
-        Eq +
-        std::ops::SubAssign<Self> +
-        std::ops::AddAssign +
-        std::ops::Shl<u8, Output = Self> +
-        std::ops::ShrAssign<u8>,
-    u8: AsPrimitive<Self>
-{
-    const _0: Self;
-    const _1: Self;
-}
-
-impl UInt for u8 {
-    const _0: u8 = 0;
-    const _1: u8 = 1;
-}
-impl UInt for u16 {
-    const _0: u16 = 0;
-    const _1: u16 = 1;
-}
-impl UInt for u32 {
-    const _0: u32 = 0;
-    const _1: u32 = 1;
-}
-impl UInt for u64 {
-    const _0: u64 = 0;
-    const _1: u64 = 1;
-}
-impl UInt for u128 {
-    const _0: u128 = 0;
-    const _1: u128 = 1;
-}
 
 pub trait Write {
     fn write<V>(&mut self, v: V) -> std::io::Result<&mut Self>
