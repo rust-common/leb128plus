a407a01c40bdb8a7197165ac4da0c9bcc645020d
Sergey Shandar <sergey.shandar@gmail.com>
2019-01-31 07:27:41 -0800

    0.7.0 explicit interface

--- src/lib.rs
+++ src/lib.rs
@@ -2,45 +2,41 @@ extern crate int;
 
 use int::UInt;
 
-pub trait Write {
-    fn write<V: UInt>(&mut self, v: V) -> std::io::Result<()>;
-}
-
-/// Write the given unsigned integer in the LEB128+ format to `std::io::Write` stream.
-///
-/// ## Examples
-///
-/// ```
-/// let f = || -> std::io::Result<Vec<u8>> {
-///     let mut v = vec![];
-///     use leb128plus::Write;
-///     {
-///         let mut c = std::io::Cursor::new(&mut v);
-///         c.write(0_u8)?;
-///         c.write(127_u16)?;
-///         c.write(128_u32)?;
-///         c.write(0xFF_u64)?;
-///         c.write(0x17F_u128)?;
-///         c.write(0x407F_u16)?;
-///         c.write(0x4080_u32)?;
-///     }
-///     Ok(v)
-/// };
-/// assert_eq!(
-///     f().unwrap(),
-///     [
-///         0,
-///         127,
-///         128, 0,
-///         0xFF, 0,
-///         0xFF, 1,
-///         0xFF, 0x7F,
-///         0x80, 0x80, 0x00
-///     ]
-/// );
-/// ```
-impl<T: std::io::Write> Write for T {
-    fn write<V: UInt>(&mut self, mut v: V) -> std::io::Result<()> {
+pub trait WriteLeb128P: std::io::Write {
+    /// Write the given unsigned integer in the LEB128+ format to `std::io::Write` stream.
+    ///
+    /// ## Examples
+    ///
+    /// ```
+    /// let f = || -> std::io::Result<Vec<u8>> {
+    ///     let mut v = vec![];
+    ///     use leb128plus::WriteLeb128P;
+    ///     {
+    ///         let mut c = std::io::Cursor::new(&mut v);
+    ///         c.write_leb128p(0_u8)?;
+    ///         c.write_leb128p(127_u16)?;
+    ///         c.write_leb128p(128_u32)?;
+    ///         c.write_leb128p(0xFF_u64)?;
+    ///         c.write_leb128p(0x17F_u128)?;
+    ///         c.write_leb128p(0x407F_u16)?;
+    ///         c.write_leb128p(0x4080_u32)?;
+    ///     }
+    ///     Ok(v)
+    /// };
+    /// assert_eq!(
+    ///     f().unwrap(),
+    ///     [
+    ///         0,
+    ///         127,
+    ///         128, 0,
+    ///         0xFF, 0,
+    ///         0xFF, 1,
+    ///         0xFF, 0x7F,
+    ///         0x80, 0x80, 0x00
+    ///     ]
+    /// );
+    /// ```
+    fn write_leb128p<V: UInt>(&mut self, mut v: V) -> std::io::Result<()> {
         loop {
             let x = v.as_();
             v >>= 7;
@@ -54,40 +50,38 @@ impl<T: std::io::Write> Write for T {
     }
 }
 
-pub trait Read {
-    fn read<V: UInt>(&mut self) -> std::io::Result<V>;
-}
+impl<T: std::io::Write> WriteLeb128P for T {}
 
-/// Read an unsigned integer in the LEB128+ format from `std::io::Read` stream.
-///
-/// ## Example
-///
-/// ```
-/// let mut c = std::io::Cursor::new(&[
-///     0,
-///     127,
-///     128, 0,
-///     0xFF, 0,
-///     0xFF, 1,
-///     0xFF, 0x7F,
-///     0x80, 0x80, 0,
-///     128,
-/// ]);
-/// use leb128plus::Read;
-/// assert_eq!(c.read::<u8>().unwrap(), 0);
-/// assert_eq!(c.read::<u16>().unwrap(), 127);
-/// assert_eq!(c.read::<u32>().unwrap(), 128);
-/// assert_eq!(c.read::<u64>().unwrap(), 0xFF);
-/// assert_eq!(c.read::<u128>().unwrap(), 0x17F);
-/// assert_eq!(c.read::<u16>().unwrap(), 0x407F);
-/// assert_eq!(c.read::<u32>().unwrap(), 0x4080);
-/// assert!(match c.read::<u64>() {
-///     Result::Err(_) => true,
-///     _ => false
-/// });
-/// ```
-impl<T: std::io::Read> Read for T {
-    fn read<V: UInt>(&mut self) -> std::io::Result<V> {
+pub trait ReadLeb128P: std::io::Read {
+    /// Read an unsigned integer in the LEB128+ format from `std::io::Read` stream.
+    ///
+    /// ## Example
+    ///
+    /// ```
+    /// let mut c = std::io::Cursor::new(&[
+    ///     0,
+    ///     127,
+    ///     128, 0,
+    ///     0xFF, 0,
+    ///     0xFF, 1,
+    ///     0xFF, 0x7F,
+    ///     0x80, 0x80, 0,
+    ///     128,
+    /// ]);
+    /// use leb128plus::ReadLeb128P;
+    /// assert_eq!(c.read_leb128p::<u8>().unwrap(), 0);
+    /// assert_eq!(c.read_leb128p::<u16>().unwrap(), 127);
+    /// assert_eq!(c.read_leb128p::<u32>().unwrap(), 128);
+    /// assert_eq!(c.read_leb128p::<u64>().unwrap(), 0xFF);
+    /// assert_eq!(c.read_leb128p::<u128>().unwrap(), 0x17F);
+    /// assert_eq!(c.read_leb128p::<u16>().unwrap(), 0x407F);
+    /// assert_eq!(c.read_leb128p::<u32>().unwrap(), 0x4080);
+    /// assert!(match c.read_leb128p::<u64>() {
+    ///     Result::Err(_) => true,
+    ///     _ => false
+    /// });
+    /// ```
+    fn read_leb128p<V: UInt>(&mut self) -> std::io::Result<V> {
         let mut result = V::_0;
         let mut shift = 0_u8;
         loop {
@@ -104,3 +98,5 @@ impl<T: std::io::Read> Read for T {
         }
     }
 }
+
+impl<T: std::io::Read> ReadLeb128P for T {}
